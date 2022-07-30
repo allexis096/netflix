ebbe0c85cdb9c04e03b47783de09193f1082f8ed
allexis <allexissoares@gmail.com>
2022-07-30 16:26:21 -0300

    feat: implement Tabbar, initial TableView and HeroImage with arch MVVM

---
+++ netflix/Controllers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  netflix
+//
+//  Created by allexis figueiredo on 27/07/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
---
+++ netflix/Controllers/UpcomingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpcomingViewController.swift
+//  netflix
+//
+//  Created by allexis figueiredo on 27/07/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+
+}
